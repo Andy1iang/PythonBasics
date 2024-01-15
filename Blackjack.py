7a5fd8a652164d8a449b1b44ec74e42ffa1a71de
Andy1iang <andyliang254@gmail.com>
2024-01-14 19:38:27 -0500

    Reformated Blackjack Simulator

--- Blackjack.py
+++ Blackjack.py
@@ -6,8 +6,15 @@ ranks = ('2 ', '3 ', '4 ', '5 ', '6 ', '7 ',
          '8 ', '9 ', '10', 'J ', 'Q ', 'K ', 'A ')
 values = {'2 ': 2, '3 ': 3, '4 ': 4, '5 ': 5, '6 ': 6, '7 ': 7,
           '8 ': 8, '9 ': 9, '10': 10, 'J ': 10, 'Q ': 10, 'K ': 10, 'A ': 1}
-blank = ['┌─────────┐', '│░░░░░░░░░│', '│░░░░░░░░░│', '│░░░░░░░░░│',
-         '│░░░░░░░░░│', '│░░░░░░░░░│', '│░░░░░░░░░│', '│░░░░░░░░░│', '└─────────┘']
+blank = ['┌─────────┐',
+         '│░░░░░░░░░│',
+         '│░░░░░░░░░│',
+         '│░░░░░░░░░│',
+         '│░░░░░░░░░│',
+         '│░░░░░░░░░│',
+         '│░░░░░░░░░│',
+         '│░░░░░░░░░│',
+         '└─────────┘']
 
 
 class Card:
@@ -16,9 +23,15 @@ class Card:
         self.rank = rank
         self.symbol = suits_symbols[suit]
         self.value = values[rank]
-        self.face = ['┌─────────┐', f'│{rank}       │', '│         │',
-                     '│         │', f'│    {suits_symbols[suit]}    │', '│         │',
-                     '│         │', f'│       {rank}│', '└─────────┘']
+        self.face = ['┌─────────┐',
+                     f'│{rank}       │',
+                     '│         │',
+                     '│         │',
+                     f'│    {suits_symbols[suit]}    │',
+                     '│         │',
+                     '│         │',
+                     f'│       {rank}│',
+                     '└─────────┘']
 
     def __str__(self):
         return (f'Suit: {self.suit} \nRank: {self.rank}')
@@ -48,7 +61,7 @@ class Player:
         self.hand_value = 0
         self.hand_image = ['', '', '', '', '', '', '', '', '']
 
-    def get_card(self, card):
+    def get_card(self, card: Card):
         if card.rank == 'A ' and self.hand_value < 11:
             self.hand_value += 11
         else:
